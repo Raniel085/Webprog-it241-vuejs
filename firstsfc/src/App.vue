ffd6eace322d4c68ba8535b99b0079ff48c3f0b1
Raniel <rqdelacruz@student.apc.edu.ph>
2026-01-17 20:26:02 +0800

    .

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <h1>Fruits</h1>
-  <food-item/>
-  <food-item2/>
+  <food-item />
+  <food-item2 />
 </template>
-
-<script></script>
-
-<style></style>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -5,5 +5,9 @@ import FoodItem from './Components/FoodItem.vue'
 import FoodItem2 from './Components/FoodItem2.vue'
 
 const app = createApp(App)
+
+// Register BOTH components so they can be used in App.vue
+app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
+
 app.mount('#app')
